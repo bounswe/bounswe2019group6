70f181f34a27ce4dd3357c7b4b967a61eab736e9
enozcan <44522401+enozcan@users.noreply.github.com>
2019-04-30 01:25:43 +0300

    Create events view

--- practice-app/src/djangotut/sadullahapp/views.py
+++ practice-app/src/djangotut/sadullahapp/views.py
@@ -7,7 +7,50 @@ from .models import Article
 import tweepy 
 import json
 
+import requests
+import datetime
+from tabulate import tabulate
+
 # Create your views here.
+def events_view(request):
+    class Event:
+        def __init__(self, data):
+            self.id = data['CalendarId']
+            self.date = data['Date'][:10]
+            self.country = data['Country']
+            self.name = data['Event']
+            self.source = data['Source']
+            self.actual = data['Actual']
+            self.previous = data['Previous']
+            self.forecasat = data['TEForecast']
+            self.importance = data['Importance']
+            self.currency = data['Currency']
+        def get_info(self):
+            lst =  {"county":self.country,"date":self.date,"rep":self.importance,"actual":self.actual,"prev":self.previous,"forecast":self.forecasat}
+            return lst
+
+    url = "https://api.tradingeconomics.com/calendar?c=guest:guest"
+     
+    response = requests.get(url)
+    data = response.text
+    parsed = json.loads(data)
+
+    tabulate_data = {}
+    
+    for i in parsed:
+        temp = Event(data = i)
+        info = temp.get_info()
+        tabulate_data[temp.name] = info
+
+    #Using JSON format
+    json_data = json.dumps(tabulate_data)
+    return JsonResponse(json_data)
+
+    #Using RENDER format
+    """context = {
+        'object_set' : tabulate_data
+    }
+    return render(request,'event_details.html',context)"""
 
 def article_detail_view(request):
     query_set = Article.objects.all()
