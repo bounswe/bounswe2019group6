9082161e426ddd76c336779e9b3ced0033160f5a
enozcan <44522401+enozcan@users.noreply.github.com>
2019-05-01 21:22:40 +0300

    Refactor code and fix "county" typo

--- practice-app/src/djangotut/eventsapp/views.py
+++ practice-app/src/djangotut/eventsapp/views.py
@@ -5,40 +5,41 @@ from django.http import JsonResponse
 import json
 import requests
 
+
+class Event:
+    def __init__(self, data):
+        self.id = data['CalendarId']
+        self.date = data['Date'][:10]
+        self.country = data['Country']
+        self.name = data['Event']
+        self.source = data['Source']
+        self.actual = data['Actual']
+        self.previous = data['Previous']
+        self.forecasat = data['TEForecast']
+        self.importance = data['Importance']
+        self.currency = data['Currency']
+    def get_info(self):
+        lst =  {"country":self.country,"date":self.date,"rep":self.importance,"actual":self.actual,"prev":self.previous,"forecast":self.forecasat}
+        return lst
+
 # Create your views here.
 def events_view(request):
-    class Event:
-        def __init__(self, data):
-            self.id = data['CalendarId']
-            self.date = data['Date'][:10]
-            self.country = data['Country']
-            self.name = data['Event']
-            self.source = data['Source']
-            self.actual = data['Actual']
-            self.previous = data['Previous']
-            self.forecasat = data['TEForecast']
-            self.importance = data['Importance']
-            self.currency = data['Currency']
-        def get_info(self):
-            lst =  {"county":self.country,"date":self.date,"rep":self.importance,"actual":self.actual,"prev":self.previous,"forecast":self.forecasat}
-            return lst
-
+    
     url = "https://api.tradingeconomics.com/calendar?c=guest:guest"
      
     response = requests.get(url)
     data = response.text
     parsed = json.loads(data)
 
-    tabulate_data = {}
+    events_data = {}
     
     for i in parsed:
         temp = Event(data = i)
         info = temp.get_info()
-        tabulate_data[temp.name] = info
+        events_data[temp.name] = info
 
     #Using JSON format
-    json_data = json.dumps(tabulate_data)
-    return JsonResponse(json_data,safe = False)
+    return JsonResponse(events_data)
 
     #Using RENDER format
     """context = {
