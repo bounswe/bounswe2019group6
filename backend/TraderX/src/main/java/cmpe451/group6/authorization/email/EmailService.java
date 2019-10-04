e1ca0197e1ef80db95bfd6369bc4f12a49a9d6ff
enozcan <enes.ozcan@hazelcast.com>
2019-10-04 23:19:34 +0300

    Add email service.

---
+++ backend/TraderX/src/main/java/cmpe451/group6/authorization/email/EmailService.java
@@ -0,0 +1,47 @@
+package cmpe451.group6.authorization.email;
+
+import org.springframework.stereotype.Service;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+@Service
+public class EmailService {
+
+    public void sendmail(String receiverAdress, String subject, String content, String mimeContent, String attachFilePath) throws  MessagingException, IOException {
+
+        String sender = EmailServiceConfig.getSenderAdress();
+        String senderPw = EmailServiceConfig.getSenderPassword();
+        Properties props = EmailServiceConfig.getProps();
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(sender , senderPw);
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(sender , false));
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverAdress));
+        msg.setSubject(subject);
+        msg.setContent(content, "text/html");
+        msg.setSentDate(new Date());
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent(mimeContent, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+        MimeBodyPart attachPart = new MimeBodyPart();
+
+        if (attachFilePath != null) {
+            attachPart.attachFile(attachFilePath);
+            multipart.addBodyPart(attachPart);
+        }
+
+        msg.setContent(multipart);
+        Transport.send(msg);
+    }
+}
