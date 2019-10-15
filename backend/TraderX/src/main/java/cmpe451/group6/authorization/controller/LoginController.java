992b5a59c6c409004a9763f010099fe6eb553fb2
Enes Ozcan <enes.ozcan@hazelcast.com>
2019-10-15 21:26:13 +0300

    Move login request parameters to request body.

--- backend/TraderX/src/main/java/cmpe451/group6/authorization/controller/LoginController.java
+++ backend/TraderX/src/main/java/cmpe451/group6/authorization/controller/LoginController.java
@@ -1,6 +1,8 @@
 package cmpe451.group6.authorization.controller;
 
+import cmpe451.group6.authorization.dto.LoginInfoDTO;
 import cmpe451.group6.authorization.dto.TokenWrapperDTO;
+import cmpe451.group6.authorization.dto.UserDataDTO;
 import cmpe451.group6.authorization.exception.GlobalExceptionHandlerController;
 import cmpe451.group6.authorization.service.LoginService;
 import io.swagger.annotations.*;
@@ -24,9 +26,8 @@ public class LoginController {
             @ApiResponse(code = 400, message = GlobalExceptionHandlerController.GENERIC_ERROR_RESPONSE),
             @ApiResponse(code = 401, message = "Invalid username and password pair.")})
     public TokenWrapperDTO login(
-                        @ApiParam("Username") @RequestParam String username,
-                        @ApiParam("Password") @RequestParam String password) {
-        return loginService.login(username, password);
+            @ApiParam("User Credentials") @RequestBody LoginInfoDTO loginInfoDTO) {
+        return loginService.login(loginInfoDTO);
     }
 
 }
