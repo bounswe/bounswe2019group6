0ceb011ecd222f7e101742b627767a41e4c33caa
sadullahgultekin <sadullahgultekin@gmail.com>
2019-10-20 14:30:11 +0300

    Fix validators in article page

--- frontend/TraderX/src/views/profile/components/Articles.vue
+++ frontend/TraderX/src/views/profile/components/Articles.vue
@@ -1,69 +1,94 @@
 <template>
   <div class="createPost-container">
-    <el-form ref="postForm" :model="postForm" :rules="rules" class="form-container">
-
-      <sticky :z-index="10" :class-name="'sub-navbar '+postForm.status">
+    <el-form
+      ref="postForm"
+      :model="postForm"
+      class="form-container"
+    >
+      <sticky
+        :z-index="10"
+        :class-name="'sub-navbar '+postForm.status"
+      >
         <CommentDropdown v-model="postForm.comment_disabled" />
         <!-- <PlatformDropdown v-model="postForm.platforms" />
         <SourceUrlDropdown v-model="postForm.source_uri" /> -->
-        <el-button v-loading="loading" style="margin-left: 10px;" type="success" @click="submitForm">
+        <el-button
+          v-loading="loading"
+          style="margin-left: 10px;"
+          type="success"
+          @click="submitForm"
+        >
           Publish
         </el-button>
-        <el-button v-loading="loading" type="warning" @click="draftForm">
+        <!-- <el-button v-loading="loading" type="warning" @click="draftForm">
           Draft
-        </el-button>
+        </el-button> -->
       </sticky>
 
       <div class="createPost-main-container">
-        <el-row>
-
-          <el-col :span="24">
-            <el-form-item style="margin-bottom: 40px;" prop="title">
-              <MDinput v-model="postForm.title" :maxlength="100" name="name" required>
+        <el-row :gutter="100">
+          <el-col :span="15">
+            <el-form-item
+              style="margin-bottom: 40px;"
+              prop="title"
+            >
+              <MDinput
+                v-model="postForm.title"
+                :maxlength="100"
+                name="name"
+                required
+              >
                 Title
               </MDinput>
             </el-form-item>
-
-            <div class="postInfo-container">
-              <el-row :gutter="6" justify="space-between">
-                <el-col :span="12">
-                  <el-form-item label-width="60px" label="Author:" class="postInfo-container-item">
-                    <el-select v-model="postForm.author" :remote-method="getRemoteUserList" filterable default-first-option remote placeholder="Search user">
-                      <el-option v-for="(item,index) in userListOptions" :key="item+index" :label="item" :value="item" />
-                    </el-select>
-                  </el-form-item>
-                </el-col>
-
-                <!-- <el-col :span="7">
-                  <el-form-item label-width="120px" label="Publish Time:" class="postInfo-container-item">
-                    <el-date-picker v-model="displayTime" type="datetime" format="yyyy-MM-dd HH:mm:ss" placeholder="Select date and time" />
-                  </el-form-item>
-                </el-col> -->
-
-                <el-col :span="12">
-                  <el-form-item label-width="90px" label="Importance:" class="postInfo-container-item">
-                    <el-rate
-                      v-model="postForm.importance"
-                      :max="3"
-                      :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
-                      :low-threshold="1"
-                      :high-threshold="3"
-                      style="display:inline-block"
-                    />
-                  </el-form-item>
-                </el-col>
-              </el-row>
-            </div>
+          </el-col>
+          <el-col :span="9">
+            <el-form-item
+              style="margin-top:40px;"
+              label-width="90px"
+              label="Importance:"
+              class="postInfo-container-item"
+            >
+              <el-rate
+                v-model="postForm.importance"
+                :max="3"
+                :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+                :low-threshold="1"
+                :high-threshold="3"
+                style="display:inline-block"
+              />
+            </el-form-item>
           </el-col>
         </el-row>
 
-        <el-form-item style="margin-bottom: 40px;" label-width="70px" label="Summary:">
-          <el-input v-model="postForm.content_short" :rows="1" type="textarea" class="article-textarea" autosize placeholder="Please enter the content" />
-          <span v-show="contentShortLength" class="word-counter">{{ contentShortLength }}words</span>
+        <el-form-item
+          style="margin-bottom: 40px;"
+          label-width="70px"
+          label="Summary:"
+        >
+          <el-input
+            v-model="postForm.content_short"
+            :rows="1"
+            type="textarea"
+            class="article-textarea"
+            autosize
+            placeholder="Please enter the content"
+          />
+          <span
+            v-show="contentShortLength"
+            class="word-counter"
+          >{{ contentShortLength }}words</span>
         </el-form-item>
 
-        <el-form-item prop="content" style="margin-bottom: 30px;">
-          <Tinymce ref="editor" v-model="postForm.content" :height="400" />
+        <el-form-item
+          prop="content"
+          style="margin-bottom: 30px;"
+        >
+          <Tinymce
+            ref="editor"
+            v-model="postForm.content"
+            :height="400"
+          />
         </el-form-item>
 
         <!-- <el-form-item prop="image_uri" style="margin-bottom: 30px;">
@@ -78,7 +103,6 @@
 import Tinymce from '@/components/Tinymce'
 import MDinput from '@/components/MDinput'
 import Sticky from '@/components/Sticky' // 粘性header组件
-import { validURL } from '@/utils/validate'
 import { fetchArticle } from '@/api/article'
 import { searchUser } from '@/api/remote-search'
 import { CommentDropdown } from './Dropdown'
@@ -88,8 +112,6 @@ const defaultForm = {
   title: '', // 文章题目
   content: '', // 文章内容
   content_short: '', // 文章摘要
-  source_uri: '', // 文章外链
-  image_uri: '', // 文章图片
   display_time: undefined, // 前台展示时间
   id: undefined,
   platforms: ['a-platform'],
@@ -107,42 +129,10 @@ export default {
     }
   },
   data() {
-    const validateRequire = (rule, value, callback) => {
-      if (value === '') {
-        this.$message({
-          message: rule.field + '为必传项',
-          type: 'error'
-        })
-        callback(new Error(rule.field + '为必传项'))
-      } else {
-        callback()
-      }
-    }
-    const validateSourceUri = (rule, value, callback) => {
-      if (value) {
-        if (validURL(value)) {
-          callback()
-        } else {
-          this.$message({
-            message: '外链url填写不正确',
-            type: 'error'
-          })
-          callback(new Error('外链url填写不正确'))
-        }
-      } else {
-        callback()
-      }
-    }
     return {
       postForm: Object.assign({}, defaultForm),
       loading: false,
       userListOptions: [],
-      rules: {
-        image_uri: [{ validator: validateRequire }],
-        title: [{ validator: validateRequire }],
-        content: [{ validator: validateRequire }],
-        source_uri: [{ validator: validateSourceUri, trigger: 'blur' }]
-      },
       tempRoute: {}
     }
   },
@@ -202,39 +192,57 @@ export default {
       document.title = `${title} - ${this.postForm.id}`
     },
     submitForm() {
-      console.log(this.postForm)
-      this.$refs.postForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$notify({
-            title: 'Success',
-            message: 'Your article is posted',
-            type: 'success',
-            duration: 2000
-          })
-          this.postForm.status = 'published'
-          this.loading = false
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
+      if (this.postForm.title == '') {
+        this.$notify({
+          message: 'Title cannot be empty',
+          type: 'error'
+        })
+      } else if (this.postForm.content == '') {
+        this.$notify({
+          message: 'Content cannot be empty',
+          type: 'error'
+        })
+      } else {
+        this.$refs.postForm.validate(valid => {
+          if (valid) {
+            // TODO later send a request to backend from here
+            console.log(this.postForm)
+            this.loading = true
+            this.$notify({
+              title: 'Success',
+              message: 'Your article is posted',
+              type: 'success',
+              duration: 2000
+            })
+            this.postForm.status = 'published'
+            this.loading = false
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      }
     },
     draftForm() {
-      if (this.postForm.content.length === 0 || this.postForm.title.length === 0) {
-        this.$message({
-          message: '请填写必要的标题和内容',
+      if (this.postForm.title == '') {
+        this.$notify({
+          message: 'Title cannot be empty',
           type: 'warning'
         })
-        return
+      } else if (this.postForm.content == '') {
+        this.$notify({
+          message: 'Content cannot be empty',
+          type: 'warning'
+        })
+      } else {
+        this.$notify({
+          message: 'Draft successful',
+          type: 'success',
+          showClose: true,
+          duration: 1000
+        })
+        this.postForm.status = 'draft'
       }
-      this.$message({
-        message: '保存成功',
-        type: 'success',
-        showClose: true,
-        duration: 1000
-      })
-      this.postForm.status = 'draft'
     },
     getRemoteUserList(query) {
       searchUser(query).then(response => {
