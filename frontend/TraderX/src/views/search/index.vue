064261e04304a5f238e020bf57bb89284d308055
sadullahgultekin <sadullahgultekin@gmail.com>
2019-11-02 09:23:46 +0300

    add searched users to a table

--- frontend/TraderX/src/views/search/index.vue
+++ frontend/TraderX/src/views/search/index.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
-    <el-form ref="searchForm" :model="searchForm" autocomplete="on" label-position="left">
-      <el-form-item prop="searchText">
-        <el-input ref="searchText" v-model="searchForm.searchText" placeholder="Search SMTGH" type="text" auto-complete="on" @keyup.native="liveSearch"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click.native.prevent="handleSearch">
-          Search
-        </el-button>
-      </el-form-item>
-    </el-form>
-
-    <div>
-      <li v-for="user in data" v-bind:key="user.username">
-        {{ user.username }}
-      </li>
+    <div style="margin-top: 15px;">
+      <el-input placeholder="Please input" v-model="searchText" class="input-with-select" ref="searchText" type="text" auto-complete="on" @keyup.native="liveSearch">
+        <el-select v-model="selectedFilter" slot="prepend" placeholder="">
+          <el-option label="User" value="1"></el-option>
+          <el-option label="Event" value="2"></el-option>
+          <el-option label="Trading Equipment" value="3"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search" @click.native.prevent="handleSearch">Search</el-button>
+      </el-input>
     </div>
-
+    <el-table :data="searchResult" style="width: 100%">
+      <el-table-column prop="name" label="Name" width="180"></el-table-column>
+      <el-table-column prop="privacy" label="Privacy" width="180"></el-table-column>
+      <el-table-column prop="role" label="Role" width="180"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -26,15 +24,23 @@ export default {
   props: {},
   data() {
     return {
-      searchForm: {
-        searchText : ""
-      },
-      data : []
+      searchText : "",
+      searchResult : [{'name':'no-user','privacy':'no-user','role':'no-user'}],
+      selectedFilter: "",
     }
   },
   created() {
     this.$store.dispatch('search/getAllUsers').then(() => {
-      this.data = this.$store.getters.userSearchResult
+      var res = this.$store.getters.userSearchResult
+      var temp = []
+      res.forEach(function (user) {
+        temp.push({
+          'name': user.username,
+          'privacy' : user.isPrivate,
+          'role' : user.roles[0]
+        })
+      });
+      this.searchResult = Array.from(new Set(temp.concat(this.searchResult)))
     })
   },
   mounted() {
@@ -42,24 +48,74 @@ export default {
   },
   methods: {
     handleSearch() {
-      if (this.searchForm.searchText == '') {
+      if (this.searchText == '') {
         this.$store.dispatch('search/getAllUsers').then(() => {
-          this.data = this.$store.getters.userSearchResult
+          var res = this.$store.getters.userSearchResult
+          var temp = []
+          res.forEach(function (user) {
+            temp.push({
+              'name': user.username,
+              'privacy' : user.isPrivate,
+              'role' : user.roles[0]
+            })
+          });
+          this.searchResult = Array.from(new Set(temp.concat(this.searchResult)))
         })  
       } else {
-        this.$store.dispatch('search/searchUser', this.searchForm.searchText).then(() => {
-          this.data = [this.$store.getters.userSearchResult]
+        this.$store.dispatch('search/searchUser', this.searchText).then(() => {
+          var res = this.$store.getters.userSearchResult
+          var temp = []
+          res.forEach(function (user) {
+            temp.push({
+              'name': user.username,
+              'privacy' : user.isPrivate,
+              'role' : user.roles[0]
+            })
+          });
+          this.searchResult = Array.from(new Set(temp.concat(this.searchResult)))
         })
       }
     },
     liveSearch() {
-      this.data = this.$store.getters.userSearchResult.filter(user => user.username.includes(this.searchForm.searchText))
+      //this.searchResult = this.$store.getters.userSearchResult.filter(user => user.username.includes(this.searchText))
+      var res = this.$store.getters.userSearchResult.filter(user => user.username.includes(this.searchText))
+      var temp = []
+      res.forEach(function (user) {
+        temp.push({
+          'name': user.username,
+          'privacy' : user.isPrivate,
+          'role' : user.roles[0]
+        })
+      });
+      // deleteMultipleUsers(dupArr) {
+      //   var uniqueArr = []
+      //   dupArr.forEach(function (dupUser) {
+      //     var exists = false
+      //     uniqueArr.forEach(function (uniUser) {
+      //       if(dupUser.username == uniUser.username){
+      //         exists = true
+      //       }
+      //     });
+      //     if (!exists){
+      //       uniqueArr.push(dupUser)
+      //     }
+      //   });
+      //   return uniqueArr
+      // }
+      this.searchResult = Array.from(new Set(temp.concat(this.searchResult)))
     }
+    
 
   }
+  
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .el-select .el-input {
+    width: 110px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
 </style>
