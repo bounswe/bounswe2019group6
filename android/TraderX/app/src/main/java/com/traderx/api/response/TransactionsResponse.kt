17dd8ec579b0e3b10f263c6696157c1a88f9a30a
bwqr <ruzgardeniz.08@hotmail.com>
2019-11-25 15:23:47 +0300

    layout changed according to transactions endpoint

--- android/TraderX/app/src/main/java/com/traderx/api/response/TransactionsResponse.kt
+++ android/TraderX/app/src/main/java/com/traderx/api/response/TransactionsResponse.kt
@@ -1,8 +1,11 @@
 package com.traderx.api.response
 
+import com.google.gson.annotations.SerializedName
+
 data class TransactionsResponse(
     val equipment: String,
     val transactionType: String,
     val amount: Double,
-    val date: String
+    val createdAt: String,
+    @SerializedName("user") val username: String
 )
