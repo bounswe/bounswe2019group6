113c21d25e53b8e88bb7b831e6ee1a796700220c
sadullahgultekin <sadullahgultekin@gmail.com>
2019-11-25 12:54:57 +0300

    initialize my-investments page

---
+++ frontend/TraderX/src/components/MyInvestments/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180"> </el-table-column>
+      <el-table-column prop="name" label="Name" width="180"> </el-table-column>
+      <el-table-column prop="address" label="Address"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyInvestments',
+  props: {},
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
