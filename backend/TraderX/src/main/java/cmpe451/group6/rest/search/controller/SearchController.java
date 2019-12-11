528664a8ae4e6197ccb75ee24668e0b4fb505759
Burak İkan Yıldız <burakikanyildiz@gmail.com>
2019-12-11 22:57:10 +0300

    Search equipment system is implemented

--- backend/TraderX/src/main/java/cmpe451/group6/rest/search/controller/SearchController.java
+++ backend/TraderX/src/main/java/cmpe451/group6/rest/search/controller/SearchController.java
@@ -5,19 +5,14 @@ import cmpe451.group6.rest.article.dto.ArticleDTO;
 import cmpe451.group6.rest.equipment.dto.EquipmentResponseDTO;
 import cmpe451.group6.rest.search.dto.EquipmentSearchDTO;
 import cmpe451.group6.rest.search.service.SearchService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.List;
 
 import static cmpe451.group6.authorization.exception.GlobalExceptionHandlerController.GENERIC_ERROR_RESPONSE;
@@ -36,10 +31,19 @@ public class SearchController {
     @GetMapping(value = "/equipment/byName")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_BASIC') or hasRole('ROLE_TRADER')")
-    @ApiOperation(value = "Gets equipments whose names contains the given string.  ", response = EquipmentResponseDTO.EquipmentDTO.class)
+    @ApiOperation(value = "Gets equipments whose names contains the given string.  ", response = EquipmentSearchDTO.class)
     @ApiResponses(value = { @ApiResponse(code = 400, message = GENERIC_ERROR_RESPONSE) })
-    public List<EquipmentSearchDTO> getEquipmentsByName(HttpServletRequest req) {
-        return searchService.getEquipmentsByName(util.unwrapUsername(req));
+    public List<EquipmentSearchDTO> getEquipmentsByName(@ApiParam("Equipment Name") @RequestParam String name, HttpServletRequest req) {
+        return searchService.getEquipmentsByName(name, util.unwrapUsername(req));
+    }
+
+    @GetMapping(value = "/equipment/byCode")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_BASIC') or hasRole('ROLE_TRADER')")
+    @ApiOperation(value = "Gets equipments whose code contains the given string.  ", response = EquipmentSearchDTO.class)
+    @ApiResponses(value = { @ApiResponse(code = 400, message = GENERIC_ERROR_RESPONSE) })
+    public List<EquipmentSearchDTO> getEquipmentsByCode(@ApiParam("Equipment Code") @RequestParam String code, HttpServletRequest req) {
+        return searchService.getEquipmentsByCode(code, util.unwrapUsername(req));
     }
 
 }
--- backend/TraderX/src/main/java/cmpe451/group6/rest/search/service/SearchService.java
+++ backend/TraderX/src/main/java/cmpe451/group6/rest/search/service/SearchService.java
@@ -1,10 +1,41 @@
 package cmpe451.group6.rest.search.service;
 
+import cmpe451.group6.helpers.CustomModelMapper;
+import cmpe451.group6.rest.equipment.model.Equipment;
 import cmpe451.group6.rest.search.dto.EquipmentSearchDTO;
+import cmpe451.group6.rest.search.repository.SearchEquipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SearchService {
-    public List<EquipmentSearchDTO> getEquipmentsByName(String unwrapUsername) {
+
+    @Autowired
+    private SearchEquipmentRepository searchRepository;
+
+    @Autowired
+    private CustomModelMapper modelMapper;
+
+    public List<EquipmentSearchDTO> getEquipmentsByName(String name, String requesterName) {
+        List<Equipment> equipments = searchRepository.equipmentFindByNameContainingIgnoreCase(name);
+
+        List<EquipmentSearchDTO> equipmentSearchDTOs = new ArrayList<EquipmentSearchDTO>();
+        equipments
+                .forEach(item -> equipmentSearchDTOs.add(modelMapper.map(item, EquipmentSearchDTO.class)));
+        return equipmentSearchDTOs;
+
+    }
+
+
+    public List<EquipmentSearchDTO> getEquipmentsByCode(String code, String requesterName) {
+        List<Equipment> equipments = searchRepository.equipmentFindByCodeContainingIgnoreCase(code);
+
+        List<EquipmentSearchDTO> equipmentSearchDTOs = new ArrayList<EquipmentSearchDTO>();
+        equipments
+                .forEach(item -> equipmentSearchDTOs.add(modelMapper.map(item, EquipmentSearchDTO.class)));
+        return equipmentSearchDTOs;
     }
 }
