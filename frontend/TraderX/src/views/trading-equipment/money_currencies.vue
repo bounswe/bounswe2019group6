22d885ee0999a5b3e4b707dce2790c6f507e5b4a
Irmak Guzey <irmakguzey47@gmail.com>
2019-12-13 10:35:03 +0300

    change comment structure according to backend

--- frontend/TraderX/src/views/trading-equipment/money_currencies.vue
+++ frontend/TraderX/src/views/trading-equipment/money_currencies.vue
@@ -100,24 +100,14 @@
                       <p>
                         <span class="comment-author"> {{ c.author }} </span>
                         <span class="comment-time"> {{ c.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }} - </span>
-                        <span class="comment-options"> {{ c.likes }} Likes </span>
+                        <span class="comment-options"> {{ c.likes }} Likes - </span>
+                        <span class="comment-options"> {{ c.dislikes }} Dislikes </span>
                       </p>
                       <p class="comment-text"> {{ c.comment }} </p>
                       <p class="comment-options">
-                        <a @click="showReplyCommentDialog=true;replyCommentId=c.id"> Reply </a> |
-                        <a @click="likeComment(c.id)"> <svg-icon icon-class="star" /> Like </a>
+                        <a @click="dislikeComment(c.id)"> Dislike </a> |
+                        <a @click="likeComment(c.id)"> Like </a>
                       </p>
-                      <div class="comment-replies" v-for="r in c.replies" v-bind:key="r.id" style="padding:8px 20px 0;margin-bottom:10px;">
-                        <p>
-                          <span class="comment-author"> {{ r.author }} </span>
-                          <span class="comment-time"> {{ r.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }} - </span>
-                          <span class="comment-options"> {{ r.likes }} Likes </span>
-                        </p>
-                        <p class="comment-text"> {{ r.comment }} </p>
-                        <p class="comment-options">
-                          <a @click="likeReplyComment(c.id, r.id)"> <svg-icon icon-class="star" /> Like </a>
-                        </p>
-                      </div>
                     </el-card>
                   </el-row>
                 
@@ -185,12 +175,9 @@ export default {
       chartData: {},
       showBuyEquipmentDialog: false,
       showCreateCommentDialog: false,
-      showReplyCommentDialog: false,
       buyamountinput: '',
       select: '',
       createCommentContent: '',
-      replyCommentContent: '',
-      replyCommentId: '',
       // equipmentData is expected to be: 
       // [
       //   {
@@ -289,31 +276,15 @@ export default {
           id: 1,
           timestamp: 1024316325463,
           likes: 5,
+          dislikes: 10,
           author: 'irmakguzey',
           comment: 'That’s the findings of Natixis Global Asset Management’s annual Global Portfolio Barometer, which has found that UK portfolios with significant non-sterling assets saw average performance of more than 13 per cent.',
-          replies: [
-            {
-              id: 1,
-              timestamp: 1024316325463,
-              likes: 1,
-              author: 'sadullahgultekin',
-              comment: `I think
-                        you are
-                        quite correct!`
-            },
-            {
-              id: 2,
-              timestamp: 1024316325463,
-              likes: 0,
-              author: 'irmakguzey',
-              comment: 'Thank you!'
-            },
-          ]
         },
         {
           id: 2,
           timestamp: 1056316325463,
           likes: 54,
+          dislikes: 0,
           author: 'sadullahgultekin',
           comment: 'Matthew Riley, head of research at the portfolio research and consulting group, says: “A substantial part of the explanation is currency risk which is no surprise since currency moves in 2016 were the highest since 2008 and had a large impact on the surveyed portfolios.',
           replies: []
@@ -322,6 +293,7 @@ export default {
           id: 3,
           timestamp: 1234567890123,
           likes: 15,
+          dislikes: 60,
           author: 'burakyuksel',
           comment: 'For example, he says, a UK investor with unhedged US equity exposure (in other words, without making compensating investments to counteract the risk) would have gained an extra 19 per cent return in 2016 due to the depreciation of the Pound versus the Dollar.\"For eurozone equities, this would have been around 16 per cent, and for Japanese equities this would have been 23 per cent. Currency impact was also seen in allocation funds, EM debt and high yield debt funds, which are often not hedged by advisers.\”',
           replies: []
@@ -330,16 +302,9 @@ export default {
           id: 4,
           timestamp: 1024457825463,
           likes: 500,
+          dislikes: 500,
           author: 'irmakguzey',
           comment: 'A lot of people. Foreign exchange is most commonly known as Forex and Forex is the world’s most traded market. According to CityIndex there’s an average turnover in excess of US$5.3 trillion every single day. That’s 4.24 trillion pounds at time of writing, although as will be seen that can change. A lot of different people are trading, from large companies to part-time traders operating out of their bedrooms, something that only became possible with the proliferation of the internet.',
-          replies: []
-        },
-        {
-          id: 5,
-          timestamp: 3456789341267,
-          likes: 234,
-          author: 'burakyuksel',
-          comment: 'What drives currency movements? Most people already know that the values of currencies shift, that’s why exchange rates change. And the changes in those rates are determined by multitude of traders buying currencies with other currencies and making judgements on what each is worth in relation to each other. Prices can change at incredible speed in response to news and global events. Traders look at key factors, including political and economic stability, currency intervention, monetary policy and major events such as natural disasters. How does it work? When trading Forex, currencies come in pairs, for example, sterling/US dollar. The trader predicts how the exchange rate between the two currencies will change. So, if the trader believes that US dollars will strengthen against the pound then they buy dollars, which means they are also ditching their pounds. If they are right then the value of their currency rises and they can sell for a profit. If their hunch was wrong then they lose. For example, the GBP/USD rate shows the number of dollars one pound can buy. If a trader believes the pound will increase in value against the dollar then they use dollars to buy pounds. If the exchange rate rises then they can sell the pounds back for a profit. One of the reasons Forex trading is so popular with hobbyist investors is that the markets are open pretty much 24 hours a day, following the different countries’ time zones. Will I make any money? Forex is risky. It’s so risky that many commentators have likened home traders to professional gamblers, arguing that the idea an individual can reliably predict the movements of currencies is nonsense. There are an abundance of platforms and guides and books and investment tutorials that suggest it’s possible to make a small fortune trading currencies. However, spend any time reading forums and there are hoards of bedroom Forex traders losing money day after day. It can be very expensive to make currency transactions and individual traders usually don’t have a large enough pot to make anything other than small gains.',
           replies: []
         },
       ]
@@ -486,34 +451,12 @@ export default {
       }, this)
     },
 
-    likeReplyComment(commentId, replyId) {
+    dislikeComment(commentId) {
       this.commentList.forEach(function(comment) {
         if (comment.id == commentId) {
-          comment.replies.forEach(function(reply){
-            if (reply.id == replyId) {
-              reply.likes += 1;
-            }
-          })
+          comment.dislikes += 1;
         }
       }, this)
-    },
-
-    // TODO: This should be done at the backend
-    replyComment(commentId) {
-      this.commentList.forEach(function(comment) {
-        if (comment.id == commentId) {
-          var newReply = {
-            id: comment.replies.length+1,
-            timestamp: Date.now(),
-            likes: 0,
-            author: 'current-user',
-            comment: this.replyCommentContent
-          }
-          comment.replies.push(newReply)
-        }
-      }, this)
-        this.showReplyCommentDialog = false
-        this.replyCommentContent = ''
     },
 
     // TODO connect this to backend
@@ -522,9 +465,9 @@ export default {
         id: this.commentList.length+1,
         timestamp: Date.now(),
         likes: 0,
+        dislikes: 0,
         author: 'current-user', // TODO: change this with backend added
         comment: this.createCommentContent,
-        replies: []
       }
       this.commentList.push(newComment)
       this.showCreateCommentDialog = false
