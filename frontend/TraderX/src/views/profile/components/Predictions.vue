d7dc755313f06f251cbd1dbd5b7994e1a0022d56
sadullahgultekin <sadullahgultekin@gmail.com>
2019-12-22 14:29:00 +0300

    render predictions

--- frontend/TraderX/src/views/profile/components/Predictions.vue
+++ frontend/TraderX/src/views/profile/components/Predictions.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app-container">
-
+    <el-table :data="predictionData" stripe style="width: 100%">
+      <el-table-column prop="code" label="Code"> </el-table-column>
+      <el-table-column prop="predType" label="Type"> </el-table-column>
+      <el-table-column prop="isSucceeded" label="Success"> </el-table-column>
+      <el-table-column prop="date" label="Date"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -19,7 +24,16 @@ export default {
       console.log(this.$store.getters.userInfo.username)
       this.$store.dispatch('user/getPredictionList', this.$store.getters.userInfo.username).then(() => {
         console.log(this.$store.getters.predictionList)
-        this.predictionData = this.$store.getters.predictionList
+        var res = this.$store.getters.predictionList.predictions
+        for(var i = 0; i < res.length; i++) {
+          var d = new Date(res[i].predictionDay).toDateString()
+          this.predictionData.push({
+            "code": res[i].equipmentCode,
+            "predType": res[i].predictionType,
+            "isSucceeded": res[i].isSucceeded ? 'Success' : 'Fail',
+            "date": d,
+          })
+        }
         this.$message.success('Prediction Yeeeeaaayhh!')
       }).catch(err => {
         console.log(err)
