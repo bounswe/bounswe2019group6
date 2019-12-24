2fea3bba25c5794266927d9e9caedf3115403370
burakyuksel0 <burakyuksel0@gmail.com>
2019-12-24 14:19:33 +0300

    adjusting size of alert form and table cards

--- frontend/TraderX/src/views/trading-equipment/cryptocurrencies.vue
+++ frontend/TraderX/src/views/trading-equipment/cryptocurrencies.vue
@@ -22,7 +22,7 @@
 
                 <el-card class='container-in-tab'>
                   <el-row gutter="50">
-                    <el-col :span="7">
+                    <el-col :span="8">
                       <el-card style="height: 400px;">
                         <el-form ref="alertForm">
                           <h3>Set order for: {{activeTab}}</h3>
@@ -51,7 +51,7 @@
                         </el-form>
                       </el-card>
                     </el-col>
-                    <el-col :span="17">
+                    <el-col :span="16">
                       <el-card>
                         <el-table
                           :data="alerts"
